9f6fa86c4dc1a0a5e90842b76c9a34bea9766629
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-02-21 16:10:44 +0700

    add:java file

---
+++ Week 2/Buku06.java
@@ -0,0 +1,38 @@
+
+public class Buku06 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+    
+    public Buku06() {
+
+    }
+
+    public Buku06(String jud, String pg, int hal, int stok, int har) {
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
+
+
+    void tampilInformasi() {
+        System.out.println("Judul : " + judul);
+        System.out.println("Pengarang : " + pengarang);
+        System.out.println("Jumlah halaman : " + halaman);
+        System.out.println("Stok : " + stok);
+        System.out.println("Harga : Rp " + harga);
+    }
+    void terjual (int jml) {
+        if(stok > 0){
+            stok -= jml;
+        }
+    }
+    void restock(int jml) {
+        stok += jml;
+    }
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+    
+}
