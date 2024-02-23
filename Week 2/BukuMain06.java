fe1b209b7b49bd4bc156dca19fc05f245264f928
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-02-23 15:06:10 +0700

    modified:method and object

--- Week 2/BukuMain06.java
+++ Week 2/BukuMain06.java
@@ -7,16 +7,26 @@ public class BukuMain06 {
         bk1.stok = 13;
         bk1.harga = 71000;
 
-        bk1.tampilInformasi();
         bk1.terjual(5);
         bk1.gantiHarga(60000);
-        bk1.tampilInformasi();
 
+        bk1.hargaTotal = bk1.hitungHargaTotal();
+        bk1.totalDiskon = bk1.hitungDiskon();
+        bk1.hargaBayar = bk1.hitungHargaBayar();
+        bk1.tampilInformasi();
+        
         Buku06 bk2 = new Buku06("Self Reward", "Maheera Ayesha", 160, 29, 59000);
         bk2.terjual(11);
+        bk2.hargaTotal = bk2.hitungHargaTotal();
+        bk2.totalDiskon = bk2.hitungDiskon();
+        bk2.hargaBayar = bk2.hitungHargaBayar();
         bk2.tampilInformasi();
-
+        
         Buku06 bukuDanendra = new Buku06("Tentang Kamu", "Tere Liye", 524, 5, 80000);
+        bukuDanendra.terjual(1);
+        bukuDanendra.hargaTotal = bukuDanendra.hitungHargaTotal();
+        bukuDanendra.totalDiskon = bukuDanendra.hitungDiskon();
+        bukuDanendra.hargaBayar = bukuDanendra.hitungHargaBayar();
         bukuDanendra.tampilInformasi();
     }
 }
