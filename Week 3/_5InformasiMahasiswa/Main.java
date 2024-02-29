501847cde259578c5383f282021090ebc4e500de
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-02-29 21:27:36 +0700

    modified:add average method and largest ipk method

--- Week 3/_5InformasiMahasiswa/Main.java
+++ Week 3/_5InformasiMahasiswa/Main.java
@@ -6,9 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         infoMhs[] ArrayMhs = new infoMhs[3];
-        
+
         for (int i = 0; i < ArrayMhs.length; i++) {
-            System.out.println("Masukkan data mahasiswa ke-" + (i+1));
+            System.out.println("Masukkan data mahasiswa ke-" + (i + 1));
             System.out.print("Nama : ");
             String nama = input.nextLine();
             System.out.print("NIM : ");
@@ -18,13 +18,29 @@ public class Main {
             System.out.print("IPK : ");
             double ipk = input.nextDouble();
             input.nextLine();
-            
+
             ArrayMhs[i] = new infoMhs(nama, nim, kelamin, ipk);
         }
         System.out.println();
         for (int i = 0; i < 3; i++) {
-            System.out.println("Data mahasiswa ke-" + (i+1));
+            System.out.println("Data mahasiswa ke-" + (i + 1));
             ArrayMhs[i].tampilMhs();
         }
+
+        infoMhs ipkMhs = new infoMhs();
+        double totalIP = ipkMhs.averageIpk(ArrayMhs[0].ipk, ArrayMhs[1].ipk, ArrayMhs[2].ipk);
+        System.out.println("Rata-rata IPK : " + Math.round(totalIP * 100) / 100d);
+        System.out.println();
+
+        double ipkTerbesar = ipkMhs.ipkTerbesar(ArrayMhs[0].ipk, ArrayMhs[1].ipk, ArrayMhs[2].ipk);
+        System.out.println("IPK terbesar yaitu " + ipkTerbesar + " diperoleh oleh :");
+
+        int indexIpkTerbesar = 0;
+        for (int i = 0; i < ArrayMhs.length; i++) {
+            if (ArrayMhs[i].ipk > ArrayMhs[indexIpkTerbesar].ipk) {
+                indexIpkTerbesar = i;
+            }
+        }
+        System.out.println("Nama : " + ArrayMhs[indexIpkTerbesar].nama + "\nNIM : " + ArrayMhs[indexIpkTerbesar].nim + "\nIPK : " + ArrayMhs[indexIpkTerbesar].ipk);
     }
 }
--- Week 3/_5InformasiMahasiswa/infoMhs.java
+++ Week 3/_5InformasiMahasiswa/infoMhs.java
@@ -6,6 +6,10 @@ public class infoMhs {
     public char kelamin;
     public double ipk;
 
+    public infoMhs() {
+
+    }
+
     public infoMhs(String nama, String nim, char kelamin, double ipk) {
         this.nama = nama;
         this.nim = nim;
@@ -18,7 +22,15 @@ public class infoMhs {
         System.out.println("NIM: " + nim);
         System.out.println("Kelamin: " + (kelamin == 'L' ? "Laki-laki" : "Perempuan"));
         System.out.println("IPK: " + ipk);
-        System.out.println(); 
+        System.out.println();
+    }
+
+    public double averageIpk(double ipk, double ipk2, double ipk3) {
+        return (ipk + ipk2 + ipk3) / 3;
+    }
+
+    public double ipkTerbesar(double ipk, double ipk2, double ipk3) {
+        return Math.max(ipk, Math.max(ipk2, ipk3));
     }
 }
 
