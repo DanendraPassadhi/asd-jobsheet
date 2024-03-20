6f4301efbb009eceb0d9840f893ca6e3fb732faf
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-03-20 16:24:03 +0700

    add:java file

---
+++ Week 6/Mahasiswa.java
@@ -0,0 +1,54 @@
+public class Mahasiswa {
+    String nama;
+    int thnMasuk;
+    int umur;
+    double ipk;
+
+    Mahasiswa(String n, int t, int u, double i) {
+        nama = n;
+        thnMasuk = t;
+        umur = u;
+        ipk = i;
+    }
+
+    void tampil() {
+        System.out.println("Nama = " + nama);
+        System.out.println("Tahun Masuk = " + thnMasuk);
+        System.out.println("Umur = " + umur);
+        System.out.println("IPK = " + ipk);
+    }
+
+}
+
+class DaftarMahasiswaBerprestasi {
+    Mahasiswa[] listMhs = new Mahasiswa[5];
+    int idx;
+
+    void tambah(Mahasiswa m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!!");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa m : listMhs) {
+            m.tampil();
+            System.out.println("---------------------------");
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < listMhs.length; i++) {
+            for (int j = 0; j < listMhs.length -1; j++) {
+                if (listMhs[j].ipk < listMhs[j + 1].ipk) {
+                    Mahasiswa tmp = listMhs[j];
+                    listMhs[j] = listMhs[j + 1];
+                    listMhs[j + 1] = tmp;
+                }
+            }
+        }
+    }
+}
