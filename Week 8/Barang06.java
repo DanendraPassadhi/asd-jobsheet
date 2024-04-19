96fa194d05e0828a320eee25b53c5ce51549c8a2
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-04-19 14:32:46 +0700

    add:java file

---
+++ Week 8/Barang06.java
@@ -0,0 +1,83 @@
+public class Barang06 {
+    int kode;
+    String nama;
+    String kategori;
+
+    Barang06(int kode, String nama, String kategori) {
+        this.kode = kode;
+        this.nama = nama;
+        this.kategori = kategori;
+    }
+}
+
+class Gudang06 {
+    Barang06[] tumpukan;
+    int size;
+    int top;
+
+    public Gudang06(int kapasitas) {
+        size = kapasitas;
+        tumpukan = new Barang06[size];
+        top = -1;
+    }
+
+    public boolean cekKosong() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean cekPenuh() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void tambahBarang(Barang06 brg) {
+        if (!cekPenuh()) {
+            top++;
+            tumpukan[top] = brg;
+            System.out.println("Barang " + brg.nama + " berhasil ditambahkan ke Gudang");
+        } else {
+            System.out.println("Gagal! Tumpukan barang di Gudang sudah penuh");
+        }
+    }
+
+    Barang06 ambilBarang() {
+        if (!cekKosong()) {
+            Barang06 delete = tumpukan[top];
+            top--;
+            System.out.println("Barang " + delete.nama + " diambil dari Gudang.");
+        } else {
+            System.out.println("Tumpukan barang kosong.");
+            return null;
+        }
+    }
+
+    Barang06 lihatBarangTeratas() {
+        if (!isEmpty()) {
+            Barang06 barangTeratas = tumpukan[top];
+            System.out.println("Barang teratas : " + barangTeratas.nama);
+            return barangTeratas;
+        } else {
+            System.out.println("Tumpukan barang kosong.");
+            return null;
+        }
+    }
+
+    public void tampilkanBarang() {
+        if (!cekKosong()) {
+            System.out.println("Rincian tumpukan barang di Gudang: ");
+            // for(int i = top; i >= 0;i--)
+            for (int i = 0; i <= top; i++) {
+                System.out.printf("Kode %d: %s (Kategori %s)\n", tumpukan[i].kode, tumpukan[i].nama, tumpukan[i].kategori);
+            } else {
+                System.out.println("Tumpukan baranng kosong.");
+            }
+        }
+    }
+}
