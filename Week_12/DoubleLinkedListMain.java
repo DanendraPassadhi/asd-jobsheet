96d118e7ea262b38211c65b06dfc97793cd810af
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-05-15 13:35:07 +0700

    add:java files

---
+++ Week_12/DoubleLinkedListMain.java
@@ -0,0 +1,24 @@
+package Week_12;
+
+public class DoubleLinkedListMain {
+    public static void main(String[] args) {
+        DoubleLinkedList dll = new DoubleLinkedList();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("==========================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("==========================");
+        dll.add(40, 1);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("==========================");
+        dll.clear();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("==========================");
+    }
+}
