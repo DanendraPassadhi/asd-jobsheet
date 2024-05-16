4d0097530f6ab6f110fd3f8b5b85fe8293248e2b
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-05-16 20:38:19 +0700

    feat: add methods getFirst, getLast and get

--- Week_12/DoubleLinkedListMain.java
+++ Week_12/DoubleLinkedListMain.java
@@ -39,5 +39,23 @@ public class DoubleLinkedListMain {
         dll.remove(1);
         dll.print();
         System.out.println("Size : " + dll.size());
+        dll.clear();
+
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("==========================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("==========================");
+        dll.add(40, 1);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("==========================");
+        System.out.println("Data awal pada Linked List adalah: " + dll.getFirst());
+        System.out.println("Data akhir pada Linked List adalah: " + dll.getLast());
+        System.out.println("Data indeks ke-1 pada Linked List adalah: " + dll.get(1));
     }
 }
