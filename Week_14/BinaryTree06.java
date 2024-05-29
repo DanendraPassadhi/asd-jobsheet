63e3d136b35df7ac2d24703d0e7e70bfd994fc00
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2024-05-29 15:47:56 +0700

    add:java files

---
+++ Week_14/BinaryTree06.java
@@ -0,0 +1,170 @@
+package Week_14;
+
+public class BinaryTree06 {
+    Node06 root;
+    int size;
+
+    public BinaryTree06() {
+        root = null;
+    }
+
+    boolean isEmpty() {
+        return root != null;
+    }
+
+    public void add(int data) {
+        if (!isEmpty()) {
+            root = new Node06(data);
+        } else {
+            Node06 current = root;
+            while (true) {
+                if (data < current.data) {
+                    if (current.left == null) {
+                        current.left = new Node06(data);
+                        break;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (data > current.data) {
+                    if (current.right == null) {
+                        current.right = new Node06(data);
+                        break;
+                    } else {
+                        current = current.right;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public boolean find(int data) {
+        boolean found = false;
+        Node06 current = root;
+        while (current != null) {
+            if (current.data == data) {
+                found = true;
+                break;
+            } else if (current.data > data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return found;
+    }
+
+    public void traversePreOrder(Node06 node) {
+        if (node != null) {
+            System.out.print(" " + node.data);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    public void traversePostOrder(Node06 node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.data);
+        }
+    }
+
+    public void traverseInOrder(Node06 node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.data);
+            traverseInOrder(node.right);
+        }
+    }
+
+    public Node06 getSuccessor(Node06 del) {
+        Node06 successor = del.right;
+        Node06 successorParent = del;
+        while (successor.left != null) {
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if (successor != del.right) {
+            successorParent.left = successor.right;
+            successor.right = del.right;
+        }
+        return successor;
+    }
+
+    public void delete(int data) {
+        if (!isEmpty()) {
+            System.out.println("Tree is empty!");
+            return;
+        }
+        // find node (current) that will be deleted
+        Node06 parent = root;
+        Node06 current = root;
+        boolean isLeftChild = false;
+        while (current != null) {
+            if (current.data == data) {
+                break;
+            } else if (isLeftChild) {
+                parent = current;
+                current = current.left;
+                isLeftChild = true;
+            } else if (data > current.data) {
+                parent = current;
+                current = current.right;
+                isLeftChild = false;
+            }
+        }
+
+        // deletion
+        if (current == null) {
+            System.out.println("Couldn't find data!");
+            return;
+        } else {
+            // if there is no child, simply delete it
+            if (current.left == null && current.right == null) {
+                if (current == root) {
+                    root = null;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = null;
+                    } else {
+                        parent.right = null;
+                    }
+                }
+            } else if (current.left == null) { // if there is 1 child (right)
+                if (current == root) {
+                    root = current.right;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.right;
+                    } else {
+                        parent.right = current.right;
+                    }
+                }
+            } else if (current.right == null) { // if there is 1 child (left)
+                if (current == root) {
+                    root = current.left;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.left;
+                    } else {
+                        parent.right = current.left;
+                    }
+                }
+            } else { // if there is 2 child
+                Node06 successor = getSuccessor(current);
+                if (current == root) {
+                    root = successor;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = successor;
+                    } else {
+                        parent.right = successor;
+                    }
+                }
+                successor.left = current.left;
+            }
+        }
+    }
+}
